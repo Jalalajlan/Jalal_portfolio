01a32d648fccbaf22fd1082d943a5a5733655a52
jalalajlan <jalalajlan@gmail.com>
2023-02-01 23:48:46 +0800

    Changed navbar to show up on scroll

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./navbar.scss";
 
 const Navbar = () => {
   const [mobileNav, setMobileNav] = useState(false);
+  const [isScreenScrolled, setIsScreenScrolled] = useState(false);
+
+  const changeNavbarBackgroundOnScroll = () =>
+    window.scrollY >= 90
+      ? setIsScreenScrolled(true)
+      : setIsScreenScrolled(false);
+
+  useEffect(() => {
+    changeNavbarBackgroundOnScroll();
+    window.addEventListener("scroll", changeNavbarBackgroundOnScroll);
+  });
 
   return (
-    <header>
+    <header className={isScreenScrolled ? "navbar scrolled-navbar" : "navbar"}>
       <h1>
         Jalal <span>Ajlan</span>
       </h1>
       <button
         onClick={() => setMobileNav(!mobileNav)}
-        className="mobile-nav-toggle"
-      >
-        <span className="sr-only">menu</span>
-      </button>
+        className="mobile-nav-toggle sr-only"
+      ></button>
+      <span></span>
       <nav>
         <ul className={mobileNav ? "primary-nav show" : "primary-nav"}>
           <li>
